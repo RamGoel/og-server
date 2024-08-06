9ef9d8782308015aeec89402f7dd76f7ca245643
Ram Goel <rgoel766@gmail.com>
2024-08-06 13:45:49 +0530

    cleanup: clean ts code

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const node_html_to_image_1 = __importDefault(require("node-html-to-image"));
-const path_1 = __importDefault(require("path"));
-const app = (0, express_1.default)();
-const port = process.env.PORT || 4000;
-app.get('/og-image', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { title, author, date, image, readTime, authorImage, upvotes, content } = req.query;
+const express = require('express')
+const nodeHtmlToImage = require('node-html-to-image')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/og-image', async (req, res) => {
+    const { title, author, date, readTime, authorImage, upvotes, content } =
+        req.query
+
     const htmlTemplate = `
         <html>
           <head>
@@ -54,20 +43,25 @@ app.get('/og-image', (req, res) => __awaiter(void 0, void 0, void 0, function* (
             </div>
           </body>
         </html>
-      `;
+      `
     //   <img src="${image}" alt="Article Image" class="image">
-    const convertedImage = yield (0, node_html_to_image_1.default)({
+
+    const convertedImage = await nodeHtmlToImage({
         html: htmlTemplate,
         transparent: true,
-    });
-    res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(convertedImage, 'binary');
-}));
+    })
+
+    res.writeHead(200, { 'Content-Type': 'image/png' })
+    res.end(convertedImage, 'binary')
+})
+
 // Serve the React app (assuming it's built and in the 'build' directory)
-app.use(express_1.default.static(path_1.default.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')))
+
 app.get('*', (req, res) => {
-    res.send("Server is running...");
-});
+    res.send('Server is running...')
+})
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
